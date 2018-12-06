6dcb3214db64c6ed9416e277c4b6ec7883a46414
alexminer88 <alex.miner@gmail.com>
2018-12-05 17:45:26 -0800

    Implement schema.sql

--- server/schema.sql
+++ server/schema.sql
@@ -2,11 +2,29 @@ CREATE DATABASE chat;
 
 USE chat;
 
+CREATE TABLE rooms (
+  RoomID integer PRIMARY KEY,
+  roomname char
+
+);
+
 CREATE TABLE messages (
   /* Describe your table here.*/
+  MessageID integer auto_increment PRIMARY KEY,
+  message text,
+  RoomID integer,
+  FOREIGN KEY (RoomID) REFERENCES rooms(RoomID)
+   
 );
 
 /* Create other tables and define schemas for them here! */
+
+CREATE TABLE users (
+  UserID integer auto_increment PRIMARY KEY,
+  username char,
+  MessageID integer,
+  FOREIGN KEY (MessageID) REFERENCES messages(MessageID)
+);
 
 
 
